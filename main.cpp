3eff73fd73ec1591fe9dedb38a9ea1fa3fc350df
CaiNot <303487454@qq.com>
2018-09-13 20:12:12 +0800

    巧妙地转移0

--- main.cpp
+++ main.cpp
@@ -34,6 +34,46 @@ public:
             }
         }
     }
+
+    void betterSetZeroes(vector<vector<int>> &matrix) {
+
+        bool ifFirstRowContain0 = false, ifCurrentRowContain0 = false;
+
+        for (int i = 0; i < matrix[0].size(); ++i) {
+            if (matrix[0][i] == 0) {
+                ifFirstRowContain0 = true;
+                break;
+            }
+        }
+
+        for (int j = 1; j < matrix.size(); ++j) {
+            ifCurrentRowContain0 = false;
+            for (int i = 0; i < matrix[j].size(); ++i) {
+                if (matrix[j][i] == 0) {
+                    ifCurrentRowContain0 = true;
+                    matrix[0][i] = 0;
+                }
+            }
+            if (ifCurrentRowContain0) {
+                for (int i = 0; i < matrix[j].size(); ++i) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix[0].size(); ++i) {
+            if (matrix[0][i] && ifFirstRowContain0) {
+                matrix[0][i] = 0;
+                continue;
+            } else if (matrix[0][i] == 0) {
+                for (int j = 1; j < matrix.size(); ++j) {
+                    matrix[j][i] = 0;
+                }
+            }
+        }
+
+        return;
+    }
 };
 
 int main() {
@@ -44,7 +84,7 @@ int main() {
                                                             {1, 0, 1},
                                                             {1, 1, 1}};
     Solution s;
-    s.setZeroes(matrix1);
+    s.betterSetZeroes(matrix);
 
     for (int i = 0; i < matrix1.size(); i++) {
         for (int j = 0; j < matrix1[0].size(); j++) {
